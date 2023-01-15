212ae5afe5667214d1a0f5610de263f732433b6f
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-01-15 16:18:03 +0000

    servicable breakdown frontend display

--- client/src/Breakdown.js
+++ client/src/Breakdown.js
@@ -1,18 +1,45 @@
 import React from "react";
 
+function format_time(timeInSeconds) {
+    let hours = Math.floor(timeInSeconds / 3600);
+    let minutes = Math.floor((timeInSeconds % 3600) / 60);
+    let seconds = timeInSeconds % 60;
+
+    return (hours ? (hours + "h") : "") 
+         + (minutes ? (minutes + "m") : "")
+         + (seconds ? (seconds + "s") : "")
+}
+
 function Activity(props) {
+
+    const [open, setOpen] = React.useState(false)
+
     return (
-        <div>
-            <h3>{props.title} - {props.activity.timeInSeconds}s</h3>
-            <ul>
-                {Object.keys(props.activity.subActivities)
-                    .map(name => 
-                        <li>
-                            <Activity title={name} activity={props.activity.subActivities[name]} />
-                        </li>
-                    )
-                }
-            </ul>
+        <div className="Breakdown">
+            { Object.keys(props.activity.subActivities).length > 0
+                ?   <p className="Breakdown-text" onClick={() => setOpen(o => !o)}>
+                        {props.title} ({format_time(props.activity.timeInSeconds)})
+                        {
+                            open
+                                ? <button className="Breakdown-collapse">{"v"}</button>
+                                : <button className="Breakdown-collapse">{">"}</button>
+                        }
+                    </p>
+                : <p className="Breakdown-text">{props.title} ({format_time(props.activity.timeInSeconds)})</p>
+            }
+            {
+                open &&
+                <div class="Breakdown-container">
+                    {Object.keys(props.activity.subActivities)
+                        .map(title => { return { title: title, activity: props.activity.subActivities[title] } })
+                        .filter(({_, activity}) => activity.timeInSeconds > 10)
+                        .sort((a, b) => b.activity.timeInSeconds - a.activity.timeInSeconds)
+                        .map(({title, activity}) => 
+                            <Activity title={title} activity={activity} />
+                        )
+                    }
+                </div>
+            }
         </div>
     );
 }
